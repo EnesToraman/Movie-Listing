a4bd232725975d602b5d18112404a14742f4f926
EnesToraman <enstrmn@hotmail.com>
2021-11-09 19:51:03 +0300

    Manage button state.

--- src/components/movieFeed/movieFeed.js
+++ src/components/movieFeed/movieFeed.js
@@ -5,6 +5,8 @@ import Card from '@mui/material/Card';
 
 export const MovieFeed = () => {
     const [movieData, setMovieData] = useState([]);
+    const [disabledButton, setDisabledButton] = useState([]);
+    const [isAdded, setIsAdded] = useState([])
 
     useEffect(() => {
         const fetchAPI = async () => {
@@ -13,35 +15,41 @@ export const MovieFeed = () => {
         fetchAPI();
     });
 
+    const handleButtonActivity = index => {
+        setDisabledButton([...disabledButton, index]);
+        setIsAdded([...isAdded, index])
+    }
+
     return (
         <Stack spacing={2} p={2}>
-            <Card>
-                {movieData.length &&
-                    <Grid container>
-                        <Grid className="image-grid" item xs={6} p={3}>
-                            <img src={movieData[0].Poster} alt="Movie Poster" />
-                        </Grid>
-                        <Grid item xs={6} pt={3}>
-                            <Grid className="text-button-grid" xs={12}>
-                                <Typography variant="subtitle1">{movieData[0].Title}</Typography>
-                                <Typography variant="button">{movieData[0].Year}</Typography>
+            {movieData.length &&
+                movieData.map((movie, index) =>
+                    < Card >
+                        <Grid container>
+                            <Grid className="image-grid" item xs={6} p={3}>
+                                <img src={movie.Poster} alt="Movie Poster" />
                             </Grid>
-                            <Grid className="text-button-grid" xs={12} textAlign="center" pt={4}>
-                                <Button disableRipple variant="contained" color="success" size="small">
-                                    Add to list
-                                </Button>
+                            <Grid item xs={6} pt={3}>
+                                <Grid className="text-button-grid" xs={12} pr={1}>
+                                    <Typography variant="subtitle1">{movie.Title}</Typography>
+                                    <Typography variant="button">{movie.Year}</Typography>
+                                </Grid>
+                                <Grid className="text-button-grid" xs={12} textAlign="center" pt={4}>
+                                    <Button
+                                        onClick={() => handleButtonActivity(index)}
+                                        disableRipple
+                                        disabled={disabledButton.includes(index)}
+                                        variant="contained"
+                                        color="success"
+                                        size="small">
+                                        {isAdded.includes(index) ? 'Added' : 'Add to list'}
+                                    </Button>
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
-                }
-            </Card>
-            <Card>Item 2</Card>
-            <Card>Item 3</Card>
-            <Card>Item 3</Card>
-            <Card>Item 3</Card>
-            <Card>Item 3</Card>
-            <Card>Item 3</Card>
-            <Card>Item 3</Card>
-        </Stack>
+                    </Card>
+                )
+            }
+        </Stack >
     )
 }
